a871b3f17f61835f9de073004c75b3bdccb778d2
Rick <rickmadureira8@gmail.com>
2025-05-06 14:00:19 -0300

    ouxit

--- my-app/src/pages/Home/index.jsx
+++ my-app/src/pages/Home/index.jsx
@@ -1,7 +1,21 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+
 export default function Home() {
+    const [posts, setPosts] = useState([]);
+
+    useEffect(() => {
+        axios.get("https://jsonplaceholder.typicode.com/posts?_limit=10")
+             .then((res) => setPosts(res.data))
+
+    }, [])
+
+    console.log(posts)
+
+
     return (
         <>
-        <h1 className="text-2x1 font-bold">Home</h1>
+        
         </>
     )
 }
